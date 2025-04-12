63bcf30d906db768dbd2bb6b85204cc18db8be29
Made <2020-051183@intecap.edu.gt>
2025-04-11 20:11:32 -0600

    Se sube projecto

---
+++ src/main/java/org/example/Models/EmpleadoModel.java
@@ -0,0 +1,19 @@
+package org.example.Models;
+
+public class EmpleadoModel {
+    private int noEmpleado;
+    private String nombresEmpleado;
+    private String apellidosEmpleado;
+    private int dpiEmpleado;
+    private int telefonoEmpleado;
+    private String correoEmpleado;
+    private int nitEmpleado;
+    private String direccionEmpleado;
+    private int igssEmpleado;
+    private int irtraEmpleado;
+
+    public EmpleadoModel() {
+    }
+
+
+}
